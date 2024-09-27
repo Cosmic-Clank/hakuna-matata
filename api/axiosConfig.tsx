b2e9f59f04608428375a71dd579f3b75117a9855
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2024-09-27 23:45:51 +0400

    fixing env variables

--- api/axiosConfig.tsx
+++ api/axiosConfig.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios with base URL and common headers
 const axiosInstance = axios.create({
-	baseURL: "https://khz6n3f6-8000.inc1.devtunnels.ms/", // Your backend URL
+	baseURL: process.env.EXPO_PUBLIC_BACKEND_BASEURL, // Your backend URL
 	timeout: 5000,
 });
 
